acc40a55c42f9c5e71a23d72facf2594d52209c9
SebasTyler <ibikatti@gmail.com>
2021-03-04 17:47:10 -0400

    TacksSearch done

--- src/features/home/HomeScreen.jsx
+++ src/features/home/HomeScreen.jsx
@@ -33,6 +33,7 @@ import RadioSelect from '../../globalComponents/RadioSelect/RadioSelect';
 
 import VariationLink from '../../globalComponents/VariationLink/VariationLink';
 
+
 const HomeScreen= () =>{
     const strings = HOME_SCREEN_es;
     const { buttonSearchPlaceholder, buttonTitle01, buttonTitle02 } = strings;
@@ -111,7 +112,6 @@ const HomeScreen= () =>{
             </div>
 
             <div className="container mx-auto px-4">
-
                 <div className="OrderByModal relative bg-background02 rounded-double pt-10 px-5 pb-5 my-4">
                     <div className="Global-line"></div>
                     <div className="OrderByModal__content flex flex-col">

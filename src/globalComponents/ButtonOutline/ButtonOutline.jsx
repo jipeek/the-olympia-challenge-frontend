847282b666abf217ad2e7b274f0268f4c8f63688
SebasTyler <ibikatti@gmail.com>
2021-03-04 17:35:30 -0400

    SelectOption done

--- src/globalComponents/ButtonOutline/ButtonOutline.jsx
+++ src/globalComponents/ButtonOutline/ButtonOutline.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const ButtonOutline = ( { className, id, typeButton, text } ) => {
-
     return (
         <button className={`ButtonOutline w-full text-white text-center border-2 text-regular py-3 border-primary bg-transparent font-bold rounded-double focus:outline-none focus:ring-transparent ${className}`} type={typeButton} id={id}>{text}</button>
     )

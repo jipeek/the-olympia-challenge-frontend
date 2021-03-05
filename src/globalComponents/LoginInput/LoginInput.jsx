632ff75bee76ce0e15e444f5e6f11df950299ac8
SebasTyler <ibikatti@gmail.com>
2021-03-05 11:37:10 -0400

    LoginView & SignUp done, backend missing

--- src/globalComponents/LoginInput/LoginInput.jsx
+++ src/globalComponents/LoginInput/LoginInput.jsx
@@ -5,11 +5,11 @@ import React from 'react';
   Si quieres hacer un input de password debes usar la clase: LoginInput--psswd
  ---------------------------- */
 
-const LoginInput = ( { className,  labelName, typeInput, value, placeholder } ) =>{
+const LoginInput = ( { className,  labelName, typeInput, nameInput, placeholder } ) =>{
     return (
         <div className={`LoginInput flex flex-col justify-between font-medium mb-6 ${className}`} >
             <label className="uppercase tracking-wide text-grey text-superSmall font-bold pb-2" htmlFor="">{labelName}</label>
-            <input className="LoginInput__input text-white placeholder-colorplaceholder text-medium border-0 border-b-2 py-3 pl-0 font-semibold font-sans border-background02 focus:border-colorful05 focus:outline-none focus:ring-transparent" type={typeInput} placeholder={placeholder}  />
+            <input className="LoginInput__input text-white placeholder-colorplaceholder text-medium border-0 border-b-2 py-3 pl-0 font-semibold font-sans border-background02 focus:border-colorful05 focus:outline-none focus:ring-transparent" name={nameInput} type={typeInput} placeholder={placeholder}  />
         </div>
     )
 }
